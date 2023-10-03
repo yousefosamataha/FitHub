dfbddc39da83c0abec41babaa8d62596d26b15e2
MuhammadJamal99 <muhammadjammi18@gmail.com>
2023-10-04 01:09:45 +0300

    add Localization Files and Services

---
+++ gms.common/Localization/JsonStringLocalizer.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+
+namespace gms.common.Localization;
+public class JsonStringLocalizer : IStringLocalizer
+{
+    private readonly JsonSerializer _serializer = new();
+    public LocalizedString this[string name]
+    {
+        get => new LocalizedString(name, GetString(name).Result);
+    }
+
+    public LocalizedString this[string name, params object[] arguments]
+    {
+        get
+        {
+            var actualvalue = this[name];
+            return !actualvalue.ResourceNotFound ? new LocalizedString(name, string.Format(actualvalue.Value, arguments)) : actualvalue;
+        }
+    }
+
+    public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+    {
+        string? fullFilePath = Path.GetFullPath($"/Resources/{Thread.CurrentThread.CurrentCulture.Name}.json");
+
+        using FileStream stream = new(fullFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using StreamReader streamReader = new(stream);
+        using JsonTextReader reader = new(streamReader);
+
+        while (reader.Read())
+        {
+            if (reader.TokenType != JsonToken.PropertyName)
+                continue;
+
+            string? key = reader.Value as string;
+            reader.Read();
+
+            string? value = _serializer.Deserialize<string>(reader);
+            yield return new LocalizedString(key, value);
+        }
+
+    }
+    private async Task<string> GetString(string key)
+    {
+        string? fullFilePath = Path.GetFullPath($"/Resources/{Thread.CurrentThread.CurrentCulture.Name}.json");
+        return File.Exists(fullFilePath) ? await GetStringFromJson(key, fullFilePath) : string.Empty;
+    }
+    private async Task<string> GetStringFromJson(string key, string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(filePath))
+            return string.Empty;
+
+        using FileStream stream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using StreamReader streamReader = new(stream);
+        using JsonTextReader reader = new(streamReader);
+
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonToken.PropertyName && reader.Value as string == key)
+            {
+                await reader.ReadAsync();
+                return _serializer.Deserialize<string>(reader);
+            }
+        }
+        return string.Empty;
+    }
+}
---
+++ gms.common/Localization/JsonStringLocalizerFactory.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace gms.common.Localization;
+public class JsonStringLocalizerFactory : IStringLocalizerFactory
+{
+    public IStringLocalizer Create(Type resourceSource) => new JsonStringLocalizer();
+
+    public IStringLocalizer Create(string baseName, string location) => new JsonStringLocalizer();
+}
