0321c0acdadbc2bc6bab9b50e2cadaae6a252fdc
yosef taha <yousefh33@outlook.sa>
2023-12-02 11:21:43 +0200

    Last Update

--- gms.web/wwwroot/assets/js/customJs/custom.js
+++ gms.web/wwwroot/assets/js/customJs/custom.js
@@ -1,10 +1,48 @@
-﻿
+﻿// [1] save activated tabs and navigate between them
 
-[...document.querySelectorAll('.app-sidebar-menu-primary.menu>.menu-item .menu-item .menu-link')].forEach(function (item) {
-    item.addEventListener('click', function () {
-        [...document.querySelectorAll('.app-sidebar-menu-primary.menu>.menu-item .menu-item .menu-link')].forEach(function (e) {
-            e.classList.remove("active");
+function sideMenuTabClick(e, event) {
+    sessionStorage.setItem("activated-menu-item-tab", e.dataset.menuItemTab);
+}
+
+function activeSideMenuTab() {
+    let activeSideMenuTab = sessionStorage.getItem("activated-menu-item-tab");
+    let menuItemList = document.querySelectorAll(".menu-item.menu-accordion");
+
+    menuItemList.forEach(e => {
+        e.querySelectorAll(".menu-item .menu-sub .menu-item").forEach(x => {
+            if (x.dataset.menuItemTab == activeSideMenuTab) {
+                x.querySelector(".menu-link").classList.add("active");
+                const parentWithClass = x.closest('.menu-item.menu-accordion');
+                parentWithClass.classList.add("show");
+                parentWithClass.querySelector(".menu-link").classList.add("active");
+            }
         });
-        item.classList.add("active");
     });
-});
+}
+
+// [2] save Sidebar Minimiz Status
+
+function sidebarMinimizeClick(e) {
+    if (e.classList.contains("active")) {
+        sessionStorage.setItem("sidebar-minimize", false);
+    } else {
+        sessionStorage.setItem("sidebar-minimize", true);
+    }
+}
+
+function activeSidebarMinimize() {
+    let sidebarMinimizeStatus = sessionStorage.getItem("sidebar-minimize");
+
+    if (sidebarMinimizeStatus == "true") {
+        document.getElementById("kt_app_sidebar_toggle").classList.add("active");
+        document.querySelector("body").dataset.ktAppSidebarMinimize = "on";
+    } else if (sidebarMinimizeStatus == "false") {
+        document.getElementById("kt_app_sidebar_toggle").classList.remove("active");
+        document.querySelector("body").dataset.ktAppSidebarMinimize = "";
+    }
+}
+
+
+
+activeSideMenuTab();
+activeSidebarMinimize();
