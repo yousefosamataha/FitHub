6a0422d40206ea1d9366e7c24095005c4c82f189
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-01-19 19:27:24 +0200

    Add GymLevel Entity

--- gms.data/Data/ApplicationDbContext.cs
+++ gms.data/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<GymEventPlaceEntity> GymEventPlaces { get; set; }
     public DbSet<GymGroupEntity> GymGroups { get; set; }
     public DbSet<GymInterestAreaEntity> GymInterestAreas { get; set; }
+    public DbSet<GymLevelEntity> GymLevels { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
