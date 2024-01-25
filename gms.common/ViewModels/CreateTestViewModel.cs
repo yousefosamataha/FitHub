e38814fe7abb47f677935a3f01713f447c4f8b7d
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-01-25 20:46:08 +0200

    Make Localization Work Right

---
+++ gms.common/ViewModels/CreateTestViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gms.common.ViewModels;
+public class CreateTestViewModel
+{
+    [Display(Name = "name"), Required(ErrorMessage = "required")]
+    public string Name { get; set; }
+}
