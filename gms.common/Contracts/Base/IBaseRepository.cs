921c2f4f786fadeaab63b1f95e81f087c6f678f4
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-01-28 01:21:05 +0200

    Add Base Repository

---
+++ gms.common/Contracts/Base/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using gms.common.Constants;
+using System.Linq.Expressions;
+
+namespace gms.common.Contracts.Base;
+
+public interface IBaseRepository<T> where T : class
+{
+	T GetById(Guid id);
+	Task<T> GetByIdAsync(Guid id);
+	List<T> GetAll();
+	Task<List<T>> GetAllAsync();
+	T Find(Expression<Func<T, bool>> criteria, string[] includes = null);
+	List<T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null);
+	List<T> FindAll(Expression<Func<T, bool>> criteria, int skip, int take, string[] includes = null);
+	List<T> FindAll(Expression<Func<T, bool>> criteria, int? skip, int? take, string[] includes = null, Expression<Func<T, object>> orderBy = null, string orderbyDirection = OrderBy.Asending);
+	T Add(T entity);
+	List<T> AddRange(List<T> entities);
+	T Update(T entity);
+	void Delete(T entity);
+	void DeleteRange(List<T> entities);
+	void Attach(T entity);
+	int Count();
+	int Count(Expression<Func<T, bool>> criteria);
+}
