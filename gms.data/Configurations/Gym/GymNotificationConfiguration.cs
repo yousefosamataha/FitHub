9c9794881892f3bac77d45cffee6c287354b5dc2
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-03-22 02:50:44 +0200

    Add GymNotificationEntity

---
+++ gms.data/Configurations/Gym/GymNotificationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using gms.common.Constants;
+using gms.data.Models.Gym;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace gms.data.Configurations.Gym;
+internal class GymNotificationConfiguration : IEntityTypeConfiguration<GymNotificationEntity>
+{
+    public void Configure(EntityTypeBuilder<GymNotificationEntity> builder)
+    {
+        builder.ToTable(gmsDbProperties.DbTablePrefix + ".GymNotification", gmsDbProperties.DbSchema);
+
+        builder.HasKey(gn => gn.Id);
+
+        builder.Property(gn => gn.NotificationTitle)
+               .IsRequired()
+               .HasMaxLength(256);
+
+        builder.Property(gn => gn.NotificationMessageBody).IsRequired();
+    }
+}
