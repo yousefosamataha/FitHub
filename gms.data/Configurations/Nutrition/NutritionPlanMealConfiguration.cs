03bbfa93303a7e2050599b99e33e53c3a790f02c
yosef taha <yousefh33@outlook.sa>
2024-03-23 21:39:23 +0200

    Add NutritionPlanMealEntity

---
+++ gms.data/Configurations/Nutrition/NutritionPlanMealConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+// TODO: Run Migration
+//using gms.common.Constants;
+//using gms.data.Models.Nutrition;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+//namespace gms.data.Configurations.Nutrition;
+
+//internal class NutritionPlanMealConfiguration : IEntityTypeConfiguration<NutritionPlanMealEntity>
+//{
+//	public void Configure(EntityTypeBuilder<NutritionPlanMealEntity> builder)
+//	{
+//		builder.ToTable(gmsDbProperties.DbTablePrefix + ".NutritionPlanMeal", gmsDbProperties.DbSchema);
+
+//		builder.HasKey(npm => npm.Id);
+
+//		builder.HasOne(npm => npm.NutritionPlan)
+//			   .WithMany(np => np.NutritionPlanMeals)
+//			   .HasForeignKey(npm => npm.NutritionPlanId);
+//	}
+//}
