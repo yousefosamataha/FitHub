b128c58ddd4f3e5a0e0f3600352a56ddd7fbc1a7
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-03-24 01:11:16 +0200

    Add Nutrition Plan Entity

--- gms.data/Configurations/Nutrition/NutritionPlanConfiguration.cs
+++ gms.data/Configurations/Nutrition/NutritionPlanConfiguration.cs
@@ -1,21 +1,21 @@
-﻿//using gms.common.Constants;
-//using gms.data.Models.Nutrition;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using gms.common.Constants;
+using gms.data.Models.Nutrition;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-//namespace gms.data.Configurations.Nutrition;
+namespace gms.data.Configurations.Nutrition;
 
-//internal class NutritionPlanConfiguration : IEntityTypeConfiguration<NutritionPlanEntity>
-//{
-//	public void Configure(EntityTypeBuilder<NutritionPlanEntity> builder)
-//	{
-//		builder.ToTable(gmsDbProperties.DbTablePrefix + ".NutritionPlan", gmsDbProperties.DbSchema);
+internal class NutritionPlanConfiguration : IEntityTypeConfiguration<NutritionPlanEntity>
+{
+    public void Configure(EntityTypeBuilder<NutritionPlanEntity> builder)
+    {
+        builder.ToTable(gmsDbProperties.DbTablePrefix + ".NutritionPlan", gmsDbProperties.DbSchema);
 
-//		builder.HasKey(np => np.Id);
+        builder.HasKey(np => np.Id);
 
-//		// TODO: Run Migration
-//		// builder.HasMany(np => np.NutritionPlanMeals)
-//		// 	   .WithOne(npm => npm.NutritionPlan)
-//		// 	   .HasForeignKey(npm => npm.NutritionPlanId);
-//	}
-//}
+        // TODO: Run Migration
+        // builder.HasMany(np => np.NutritionPlanMeals)
+        // 	   .WithOne(npm => npm.NutritionPlan)
+        // 	   .HasForeignKey(npm => npm.NutritionPlanId);
+    }
+}
