70ff4b9797139fb11c75fc83f52aed03baedc1dc
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-03-24 00:23:56 +0200

    Last Update

---
+++ gms.data/Models/Membership/GymMembershipEntity.cs
@@ -0,0 +1,17 @@
+﻿using gms.common.Enums;
+using gms.data.Models.Base;
+
+namespace gms.data.Models.Membership;
+public class GymMembershipEntity : BaseEntity
+{
+    public int MembershipId { get; set; }
+    public StatusEnum SubscriptionStatusId { get; set; }
+    public StatusEnum PaymentStatusId { get; set; }
+    public DateTime JoiningDate { get; set; }
+    public DateTime ExpiringDate { get; set; }
+
+    // TODO: Add Relation Entities
+    //public int MemberId { get; set; }
+    //public int CreatedById { get; set; }
+    //public virtual GymMembershipPlanEntity MembershipPlan { get; set; }
+}
---
+++ gms.data/Models/Membership/GymMembershipPlanEntity.cs
@@ -0,0 +1,23 @@
+﻿using gms.common.Enums;
+using gms.data.Models.Base;
+
+namespace gms.data.Models.Membership;
+public class GymMembershipPlanEntity : BaseEntity
+{
+    public required string MembershipName { get; set; }
+    public required string MembershipDuration { get; set; }
+    public MembershipLengthTypeEnum MembershipDurationTypeId { get; set; }
+    public decimal MembershipAmount { get; set; }
+    public StatusEnum MembershipStatusId { get; set; }
+    public decimal SignupFee { get; set; }
+    public string MembershipDescription { get; set; }
+    public int ClassIsLimit { get; set; }
+    public int ClassLimitDays { get; set; }
+    public int ClassLimitation { get; set; }
+
+    // TODO: Add Relation Entities
+    //public int CreatedById { get; set; } // FK
+    //public int InstallmentPlanId { get; set; } // FK
+    //public decimal InstallmentAmount { get; set; }
+    //public ICollection<MembershipPlanClass> MembershipPlanClasses { get; set; }
+}
