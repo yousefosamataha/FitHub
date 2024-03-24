4da9b582d01f9bc88b349d69bba7be5e743157da
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-03-24 02:18:09 +0200

    Add Gym Member Membership Entity

---
+++ gms.data/Configurations/Membership/GymMemberMembershipConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using gms.common.Constants;
+using gms.data.Models.Membership;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace gms.data.Configurations.Membership;
+internal class GymMemberMembershipConfiguration : IEntityTypeConfiguration<GymMemberMembershipEntity>
+{
+    public void Configure(EntityTypeBuilder<GymMemberMembershipEntity> builder)
+    {
+        builder.ToTable(gmsDbProperties.DbTablePrefix + ".GymMemberMembership", gmsDbProperties.DbSchema);
+
+        builder.HasKey(gmm => gmm.Id);
+
+        builder.HasMany(gmm => gmm.MembershipPaymentHistories)
+               .WithOne(gmph => gmph.GymMemberMembership)
+               .HasForeignKey(gmph => gmph.GymMemberMembershipId);
+    }
+}
--- gms.data/Models/Membership/GymMembershipPaymentHistoryEntity.cs
+++ gms.data/Models/Membership/GymMembershipPaymentHistoryEntity.cs
@@ -4,12 +4,14 @@ using gms.data.Models.Base;
 namespace gms.data.Models.Membership;
 public class GymMembershipPaymentHistoryEntity : BaseEntity
 {
+    public int GymMemberMembershipId { get; set; }
     public decimal PaidAmount { get; set; }
     public PaymentMethodEnum PaymentMethodId { get; set; }
     public string TransactionId { get; set; }
 
+    public virtual GymMemberMembershipEntity GymMemberMembership { get; set; }
+
     // TODO: Add Relation Entities
-    //public GymMemberMembership GymMemberMembership { get; set; }
     //public int GymId { get; set; } // FK
     //public int CreatedById { get; set; }
 }
