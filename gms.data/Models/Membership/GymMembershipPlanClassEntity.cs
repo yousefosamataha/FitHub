47cf358019c941e97bc7b5c8897caefd1d19330a
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-04-01 10:26:48 +0200

    Add GymMembershipPlanClass Entity

---
+++ gms.data/Models/Membership/GymMembershipPlanClassEntity.cs
@@ -0,0 +1,10 @@
+﻿//using gms.data.Models.Base;
+
+
+//namespace gms.data.Models.Membership;
+//public class GymMembershipPlanClassEntity : BaseEntity
+//{
+//    public int MembershipId { get; set; }
+//    public int ClassId { get; set; }
+//    public virtual GymMembershipPlanEntity MembershipPlan { get; set; }
+//}
