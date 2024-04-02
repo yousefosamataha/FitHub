68aa7842db99b9c0ac27802f569d53501e043297
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-04-02 13:45:53 +0200

    Add MemberShip Repositories

---
+++ gms.service/Membership/GymMembershipPaymentHistoryRepository/GymMembershipPaymentHistoryService.cs
@@ -0,0 +1,13 @@
+﻿using gms.data;
+using gms.data.Models.Membership;
+using gms.services.Base;
+
+namespace gms.service.Membership.GymMembershipPaymentHistoryRepository;
+public class GymMembershipPaymentHistoryService : BaseRepository<GymMembershipPaymentHistoryEntity>, IGymMembershipPaymentHistoryService
+{
+    private readonly ApplicationDbContext _context;
+    public GymMembershipPaymentHistoryService(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
---
+++ gms.service/Membership/GymMembershipPaymentHistoryRepository/IGymMembershipPaymentHistoryService.cs
@@ -0,0 +1,7 @@
+﻿using gms.data.Models.Membership;
+using gms.services.Base;
+
+namespace gms.service.Membership.GymMembershipPaymentHistoryRepository;
+public interface IGymMembershipPaymentHistoryService : IBaseRepository<GymMembershipPaymentHistoryEntity>
+{
+}
