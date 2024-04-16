6ccae50ac76de47394d566431b5ac431ca40f35d
yosef taha <yousefh33@outlook.sa>
2024-04-16 13:40:09 +0200

    Gym Category Edit

--- gms.data/Models/Gym/GymGeneralSettingEntity.cs
+++ gms.data/Models/Gym/GymGeneralSettingEntity.cs
@@ -4,21 +4,23 @@ namespace gms.data.Models.Gym;
 
 public class GymGeneralSettingEntity : BaseEntity
 {
-    public byte[] GymLogo { get; set; }
-    public string? Weight { get; set; }
-    public string? Height { get; set; }
-    public string? Chest { get; set; }
-    public string? Waist { get; set; }
-    public string? Thing { get; set; }
-    public string? Arms { get; set; }
-    public string? Fat { get; set; }
-    public int ReminderDays { get; set; }
-    public string ReminderMessage { get; set; }
-    public bool IsShared { get; set; }
-    public byte[] ReportHeader { get; set; }
-    public byte[] ReportFooter { get; set; }
-    public int? GymId { get; set; }
+    public int GymId { get; set; }
     public int? BranchId { get; set; }
+    public byte[]? GymLogo { get; set; }
+    public string Weight = "KG";
+    public string Height = "Centimeter";
+    public string Chest = "Inches";
+    public string Waist = "Inches";
+    public string Thing = "Inches";
+    public string Arms = "Inches";
+    public string Fat = "Percentage";
+    public int ReminderDays = 5;
+    public string ReminderMessage = "Hello {0},\r\n      Your Membership  {1}  started at {2} and it will expire on {3}.\r\nThank You.";
+    public bool IsShared { get; set; }
+    public byte[]? ReportHeader { get; set; }
+    public byte[]? ReportFooter { get; set; }
+
+    // Navigation properties
     public virtual GymEntity Gym { get; set; }
     public virtual GymBranchEntity GymBranch { get; set; }
 }
