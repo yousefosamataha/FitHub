c7440de4c18cf93b4e8796ff43864050e65cf29e
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-04-27 11:26:56 +0300

    Last Update

---
+++ gms.service/TestUser/GymUserService.cs
@@ -0,0 +1,21 @@
+﻿using gms.common.Models.Identity;
+using gms.data;
+using gms.data.Mapper.Identity;
+using gms.data.Models.Identity;
+
+namespace gms.service.TestUser;
+public class GymUserService : IGymUserService
+{
+    private readonly ApplicationDbContext _context;
+    public GymUserService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GymUserDto> UpdateGymUser(GymUserEntity entity)
+    {
+        _context.Users.Update(entity);
+        await _context.SaveChangesAsync();
+        return entity.ToDTO();
+    }
+}
---
+++ gms.service/TestUser/IGymUserService.cs
@@ -0,0 +1,9 @@
+﻿using gms.common.Models.Identity;
+using gms.data.Models.Identity;
+
+namespace gms.service.TestUser;
+
+public interface IGymUserService
+{
+    public Task<GymUserDto> UpdateGymUser(GymUserEntity entity);
+}
