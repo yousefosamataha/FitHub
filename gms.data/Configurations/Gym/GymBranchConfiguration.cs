ca5bff7f98bac3d7b73f3179b555bd9ba48023b2
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-05-10 14:24:01 +0300

    Add GymBranch Entity to Gym Roles Entity

--- gms.data/Configurations/Gym/GymBranchConfiguration.cs
+++ gms.data/Configurations/Gym/GymBranchConfiguration.cs
@@ -7,84 +7,88 @@ namespace gms.data.Configurations;
 
 internal class GymBranchConfiguration : IEntityTypeConfiguration<GymBranchEntity>
 {
-    public void Configure(EntityTypeBuilder<GymBranchEntity> builder)
-    {
-        builder.ToTable(gmsDbProperties.DbTablePrefix + ".GymBranch", gmsDbProperties.DbSchema);
+	public void Configure(EntityTypeBuilder<GymBranchEntity> builder)
+	{
+		builder.ToTable(gmsDbProperties.DbTablePrefix + ".GymBranch", gmsDbProperties.DbSchema);
 
-        builder.HasKey(gb => gb.Id);
+		builder.HasKey(gb => gb.Id);
 
-        builder.Property(gb => gb.BranchName).IsRequired().HasMaxLength(256);
+		builder.Property(gb => gb.BranchName).IsRequired().HasMaxLength(256);
 
-        builder.Property(gb => gb.Address).IsRequired();
+		builder.Property(gb => gb.Address).IsRequired();
 
-        builder.Property(gb => gb.ContactNumber).IsRequired();
+		builder.Property(gb => gb.ContactNumber).IsRequired();
 
-        builder.Property(gb => gb.Email).IsRequired();
+		builder.Property(gb => gb.Email).IsRequired();
 
-        builder.HasOne(gb => gb.Gym)
-               .WithMany(g => g.GymBranches)
-               .HasForeignKey(gb => gb.GymId);
+		builder.HasOne(gb => gb.Gym)
+			   .WithMany(g => g.GymBranches)
+			   .HasForeignKey(gb => gb.GymId);
 
-        builder.HasOne(gb => gb.Country)
-               .WithMany()
-               .HasForeignKey(gb => gb.CountryId);
+		builder.HasOne(gb => gb.Country)
+			   .WithMany()
+			   .HasForeignKey(gb => gb.CountryId);
 
-        builder.HasMany(gb => gb.GymUsers)
-               .WithOne(gu => gu.GymBranch)
-               .HasForeignKey(gu => gu.BranchId);
+		builder.HasMany(gb => gb.GymUsers)
+			   .WithOne(gu => gu.GymBranch)
+			   .HasForeignKey(gu => gu.BranchId);
 
-        builder.HasMany(gb => gb.GymSpecializations)
-               .WithOne(gs => gs.GymBranch)
-               .HasForeignKey(gs => gs.BranchId);
+		builder.HasMany(gb => gb.GymRoles)
+			   .WithOne(gr => gr.GymBranch)
+			   .HasForeignKey(gr => gr.BranchId);
 
-        builder.HasMany(gb => gb.GymMembershipPlans)
-               .WithOne(gmp => gmp.GymBranch)
-               .HasForeignKey(gmp => gmp.BranchId);
+		builder.HasMany(gb => gb.GymSpecializations)
+			   .WithOne(gs => gs.GymBranch)
+			   .HasForeignKey(gs => gs.BranchId);
 
-        builder.HasMany(gb => gb.ClassSchedules)
-               .WithOne(cs => cs.GymBranch)
-               .HasForeignKey(cs => cs.BranchId);
+		builder.HasMany(gb => gb.GymMembershipPlans)
+			   .WithOne(gmp => gmp.GymBranch)
+			   .HasForeignKey(gmp => gmp.BranchId);
 
-        builder.HasMany(gb => gb.GymLocations)
-               .WithOne(gl => gl.GymBranch)
-               .HasForeignKey(gl => gl.BranchId);
+		builder.HasMany(gb => gb.ClassSchedules)
+			   .WithOne(cs => cs.GymBranch)
+			   .HasForeignKey(cs => cs.BranchId);
 
-        builder.HasMany(gb => gb.GymGroups)
-               .WithOne(ggr => ggr.GymBranch)
-               .HasForeignKey(ggr => ggr.BranchId);
+		builder.HasMany(gb => gb.GymLocations)
+			   .WithOne(gl => gl.GymBranch)
+			   .HasForeignKey(gl => gl.BranchId);
 
-        builder.HasMany(gb => gb.Activities)
-               .WithOne(a => a.GymBranch)
-               .HasForeignKey(a => a.BranchId);
+		builder.HasMany(gb => gb.GymGroups)
+			   .WithOne(ggr => ggr.GymBranch)
+			   .HasForeignKey(ggr => ggr.BranchId);
 
-        builder.HasMany(gb => gb.ActivityCategories)
-               .WithOne(ac => ac.GymBranch)
-               .HasForeignKey(ac => ac.BranchId);
+		builder.HasMany(gb => gb.Activities)
+			   .WithOne(a => a.GymBranch)
+			   .HasForeignKey(a => a.BranchId);
 
-        builder.HasMany(gb => gb.WorkoutPlans)
-               .WithOne(wp => wp.GymBranch)
-               .HasForeignKey(wp => wp.BranchId);
+		builder.HasMany(gb => gb.ActivityCategories)
+			   .WithOne(ac => ac.GymBranch)
+			   .HasForeignKey(ac => ac.BranchId);
 
-        builder.HasMany(gb => gb.NutritionPlans)
-               .WithOne(np => np.GymBranch)
-               .HasForeignKey(np => np.BranchId);
+		builder.HasMany(gb => gb.WorkoutPlans)
+			   .WithOne(wp => wp.GymBranch)
+			   .HasForeignKey(wp => wp.BranchId);
 
-        builder.HasMany(gb => gb.MealTimes)
-               .WithOne(mt => mt.GymBranch)
-               .HasForeignKey(mt => mt.BranchId);
+		builder.HasMany(gb => gb.NutritionPlans)
+			   .WithOne(np => np.GymBranch)
+			   .HasForeignKey(np => np.BranchId);
 
-        builder.HasMany(gb => gb.GymNotifications)
-               .WithOne(gn => gn.GymBranch)
-               .HasForeignKey(gn => gn.BranchId);
+		builder.HasMany(gb => gb.MealTimes)
+			   .WithOne(mt => mt.GymBranch)
+			   .HasForeignKey(mt => mt.BranchId);
 
-        builder.HasMany(gb => gb.GymEventReservations)
-               .WithOne(gre => gre.GymBranch)
-               .HasForeignKey(gre => gre.BranchId);
+		builder.HasMany(gb => gb.GymNotifications)
+			   .WithOne(gn => gn.GymBranch)
+			   .HasForeignKey(gn => gn.BranchId);
 
-        builder.HasOne(gb => gb.GeneralSetting)
-               .WithMany(gg => gg.GymBranches)
-               .HasForeignKey(gb => gb.GeneralSettingId);
+		builder.HasMany(gb => gb.GymEventReservations)
+			   .WithOne(gre => gre.GymBranch)
+			   .HasForeignKey(gre => gre.BranchId);
 
-        builder.HasQueryFilter(gb => gb.IsDeleted == false);
-    }
+		builder.HasOne(gb => gb.GeneralSetting)
+			   .WithMany(gg => gg.GymBranches)
+			   .HasForeignKey(gb => gb.GeneralSettingId);
+
+		builder.HasQueryFilter(gb => gb.IsDeleted == false);
+	}
 }
--- gms.data/Models/Identity/GymIdentityRoleEntity.cs
+++ gms.data/Models/Identity/GymIdentityRoleEntity.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using gms.data.Models.Gym;
+using Microsoft.AspNetCore.Identity;
 
 namespace gms.data.Models.Identity;
 public class GymIdentityRoleEntity : IdentityRole<int>
 {
+	public int BranchId { get; set; }
+
+	public virtual GymBranchEntity GymBranch { get; set; }
 }
