d9db1dd8405341a723c7c32602fb27c775bd1c08
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-05-11 23:48:27 +0300

    Last Update

---
+++ gms.common/Permissions/NutritionPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace gms.common.Permissions;
+
+public static class NutritionPermissions
+{
+	public const string Create = $"{PermissionsConstants.Permissions}.Nutrition.Create";
+	public const string Edit = $"{PermissionsConstants.Permissions}.Nutrition.Edit";
+	public const string Delete = $"{PermissionsConstants.Permissions}.Nutrition.Delete";
+	public const string View = $"{PermissionsConstants.Permissions}.Nutrition.View";
+}
