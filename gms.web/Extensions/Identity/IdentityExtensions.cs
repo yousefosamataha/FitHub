b31d0b25a8fe44b6b1738fc54dabc5d632d9b097
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-05-23 10:02:04 +0300

    Add Extensions Methodes

---
+++ gms.web/Extensions/Identity/IdentityExtensions.cs
@@ -0,0 +1,28 @@
+﻿using gms.data.Models.Identity;
+using gms.data;
+using Microsoft.AspNetCore.Identity;
+
+namespace gms.web.Extensions.Identity;
+
+public static class IdentityExtensions
+{
+    public static void AddIdentityConfiguration(this IServiceCollection services)
+    {
+        services.AddIdentity<GymUserEntity, GymIdentityRoleEntity>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultUI();
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = "/Identity/Account/Login";
+            options.LogoutPath = "/Identity/Account/Logout";
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            options.SlidingExpiration = true;
+        });
+
+        services.Configure<SecurityStampValidatorOptions>(options =>
+        {
+            options.ValidationInterval = TimeSpan.Zero;
+        });
+    }
+}
