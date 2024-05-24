e7d97ad889332628dcb62193387e38d7544737fa
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-05-24 19:23:50 +0300

    Last Update

--- gms.web/Extensions/HangFire/HangFireJobsExtensions.cs
+++ gms.web/Extensions/HangFire/HangFireJobsExtensions.cs
@@ -1,16 +1,5 @@
-﻿using gms.service.Background;
-using Hangfire;
-
-namespace gms.web.Extensions.HangFire;
+﻿namespace gms.web.Extensions.HangFire;
 
 public static class HangFireJobsExtensions
 {
-	public static void AddHangFireJobs()
-	{
-		// Schedule the job
-		RecurringJob.AddOrUpdate<MembershipExpirationJob>(
-			job => job.CheckExpiringMembershipsAsync(),
-			Cron.Hourly);
-	}
-
 }
